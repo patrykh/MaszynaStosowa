2287d67667e2dfe399d7ed0fc599ef5676b1c075
Patryk Hołda <holda.patryk@gmail.com>
2017-03-13 21:28:54 +0100

    Begin

---
+++ MaszynaStosowa/MaszynaStosowa.cpp
@@ -0,0 +1,14 @@
+// MaszynaStosowa.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello wordl!";
+    return 0;
+}
+
